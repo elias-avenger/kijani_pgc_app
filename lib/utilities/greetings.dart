aaff58aafd24360bb69c278dd2a5cdf8b977cbc5
katoemma <emmanuelkato39@gmail.com>
2025-04-01 14:06:31 +0300

    ADD: added code to convert  data into text

--- lib/utilities/greetings.dart
+++ lib/utilities/greetings.dart
@@ -10,3 +10,31 @@ class Greetings {
     }
   }
 }
+
+  // static String _formattedDate() {
+  //   final now = DateTime.now();
+  //   return "${_getWeekday(now.weekday)}, ${now.day.toString().padLeft(2, '0')} ${_getMonth(now.month)}";
+  // }
+
+  // static String _getWeekday(int weekday) {
+  //   const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  //   return weekdays[weekday - 1];
+  // }
+
+  // static String _getMonth(int month) {
+  //   const months = [
+  //     'Jan',
+  //     'Feb',
+  //     'Mar',
+  //     'Apr',
+  //     'May',
+  //     'Jun',
+  //     'Jul',
+  //     'Aug',
+  //     'Sep',
+  //     'Oct',
+  //     'Nov',
+  //     'Dec'
+  //   ];
+  //   return months[month - 1];
+  // }
