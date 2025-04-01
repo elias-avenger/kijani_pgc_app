e459c6a8b9d6646df09408d6ce99d204df54d4d4
katoemma <emmanuelkato39@gmail.com>
2025-04-01 14:16:59 +0300

    CREATE: created a class to manage dashboard tile data

---
+++ lib/models/dashboard_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardTileData {
+  final String title;
+  final IconData icon;
+  final int count;
+  final Color color;
+
+  DashboardTileData({
+    required this.title,
+    required this.icon,
+    required this.count,
+    required this.color,
+  });
+}
