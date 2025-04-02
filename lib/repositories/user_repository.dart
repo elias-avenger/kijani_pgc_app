126a1f9fefa69ee920892a5ddffbf1abe45803ef
katoemma <emmanuelkato39@gmail.com>
2025-04-02 14:47:14 +0300

    UPDATE: added the getdata method

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -2,15 +2,17 @@ import 'package:airtable_crud/airtable_plugin.dart';
 import 'package:flutter/foundation.dart';
 import 'package:kijani_pmc_app/models/user_model.dart';
 import 'package:kijani_pmc_app/services/airtable_services.dart';
+import 'package:kijani_pmc_app/services/local_storage.dart';
 
 class UserRepository {
+  final LocalStorage _localStorage = LocalStorage();
   //check if user exists
   Future<User?> checkUser({required String email, required String code}) async {
     try {
       String filter =
           'AND({Email}="$email", {AppCode}="$code", {Status}="Active")';
       List<AirtableRecord> data =
-          await uGGardens.fetchRecordsWithFilter(kPGCsTable, filter);
+          await uGGardensBase.fetchRecordsWithFilter(kPGCsTable, filter);
 
       if (data.isNotEmpty) {
         AirtableRecord record = data.first;
@@ -32,4 +34,8 @@ class UserRepository {
   }
 
   //get user data from local storage
+  Future<User> getBranchData() async {
+    final storedData = await _localStorage.getData(key: 'userData');
+    return User.fromJson(storedData);
+  }
 }
