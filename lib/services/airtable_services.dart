0996456839551042827dc0bf50160928d2e3c4d4
katoemma <emmanuelkato39@gmail.com>
2025-04-02 14:48:51 +0300

    UPDATE: added current nursery action base

--- lib/services/airtable_services.dart
+++ lib/services/airtable_services.dart
@@ -4,6 +4,9 @@ import 'package:airtable_crud/airtable_plugin.dart';
 String apiKey = airtableAccessToken;
 
 String kUGGardensBaseID = "appJBOIeM2ZA5nhnV";
+String kUGCurrentGardensBaseID = "appoW7X8Lz3bIKpEE";
 String kPGCsTable = "PGCs";
+String kPGCReportTable = "PGC Report";
 
-AirtableCrud uGGardens = AirtableCrud(apiKey, kUGGardensBaseID);
+AirtableCrud uGGardensBase = AirtableCrud(apiKey, kUGGardensBaseID);
+AirtableCrud currentGardensBase = AirtableCrud(apiKey, kUGCurrentGardensBaseID);
