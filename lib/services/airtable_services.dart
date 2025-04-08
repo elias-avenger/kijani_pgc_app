846d1e96a35d06978434751372e7047184c9bdcf
katoemma <emmanuelkato39@gmail.com>
2025-04-08 16:22:21 +0300

    UPDATE: added the UG operations table

--- lib/services/airtable_services.dart
+++ lib/services/airtable_services.dart
@@ -3,6 +3,9 @@ import 'package:airtable_crud/airtable_plugin.dart';
 
 String apiKey = airtableAccessToken;
 
+String kUGOperationsBaseID = "aappcSe8EQxMbJSWfp";
+String kParishesTable = "Geo_Parishes";
+
 String kUGGardensBaseID = "appJBOIeM2ZA5nhnV";
 String kUGCurrentGardensBaseID = "appoW7X8Lz3bIKpEE";
 String kPGCsTable = "PGCs";
@@ -10,3 +13,4 @@ String kPGCReportTable = "PGC Report";
 
 AirtableCrud uGGardensBase = AirtableCrud(apiKey, kUGGardensBaseID);
 AirtableCrud currentGardensBase = AirtableCrud(apiKey, kUGGardensBaseID);
+AirtableCrud uGOperationsBase = AirtableCrud(apiKey, kUGOperationsBaseID);
