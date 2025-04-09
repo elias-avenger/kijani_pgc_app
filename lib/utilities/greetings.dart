5625213d572293b2c99a77ce1fbc67626bb63600
katoemma <emmanuelkato39@gmail.com>
2025-04-09 10:11:43 +0300

    UPDATE: added a function to return formated date

--- lib/utilities/greetings.dart
+++ lib/utilities/greetings.dart
@@ -9,33 +9,32 @@ class Greetings {
       return 'Good Evening';
     }
   }
+
+  String formattedDate() {
+    final now = DateTime.now();
+    return "${_getWeekday(now.weekday)}, ${now.day.toString().padLeft(2, '0')} ${_getMonth(now.month)}";
+  }
+
+  static String _getWeekday(int weekday) {
+    const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    return weekdays[weekday - 1];
+  }
+
+  static String _getMonth(int month) {
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
+    return months[month - 1];
+  }
 }
-
-  // static String _formattedDate() {
-  //   final now = DateTime.now();
-  //   return "${_getWeekday(now.weekday)}, ${now.day.toString().padLeft(2, '0')} ${_getMonth(now.month)}";
-  // }
-
-  // static String _getWeekday(int weekday) {
-  //   const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  //   return weekdays[weekday - 1];
-  // }
-
-  // static String _getMonth(int month) {
-  //   const months = [
-  //     'Jan',
-  //     'Feb',
-  //     'Mar',
-  //     'Apr',
-  //     'May',
-  //     'Jun',
-  //     'Jul',
-  //     'Aug',
-  //     'Sep',
-  //     'Oct',
-  //     'Nov',
-  //     'Dec'
-  //   ];
-  //   return months[month - 1];
-  // }
-
