c84a345373e961636aeab942d55909dc50b69c49
katoemma <emmanuelkato39@gmail.com>
2025-04-09 10:10:57 +0300

    CREATE; added an object model to handle gridItem data for the reusable screen

---
+++ lib/models/grid_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GridItemModel {
+  final String title;
+  final int value;
+  final IconData? icon;
+  final Color color;
+
+  GridItemModel({
+    required this.title,
+    required this.value,
+    required this.icon,
+    required this.color,
+  });
+}
