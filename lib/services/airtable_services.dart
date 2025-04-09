8214277b610c9a9956222f1e5f2e10e59b0ee782
katoemma <emmanuelkato39@gmail.com>
2025-04-09 13:27:47 +0300

    FIX: update the current gardens to focus on it is baseID

--- lib/services/airtable_services.dart
+++ lib/services/airtable_services.dart
@@ -9,8 +9,9 @@ String kParishesTable = "Geo_Parishes";
 String kUGGardensBaseID = "appJBOIeM2ZA5nhnV";
 String kUGCurrentGardensBaseID = "appoW7X8Lz3bIKpEE";
 String kPGCsTable = "PGCs";
-String kPGCReportTable = "PGC Report";
+String kPGCReportTable =
+    "PGC Report App Test"; //TOD: change the table name to the correct one
 
 AirtableCrud uGGardensBase = AirtableCrud(apiKey, kUGGardensBaseID);
-AirtableCrud currentGardensBase = AirtableCrud(apiKey, kUGGardensBaseID);
+AirtableCrud currentGardensBase = AirtableCrud(apiKey, kUGCurrentGardensBaseID);
 AirtableCrud uGOperationsBase = AirtableCrud(apiKey, kUGOperationsBaseID);
