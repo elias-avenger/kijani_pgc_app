fc2f417f236b8724c6ad36d0613432ef3997a984
elias-avenger <eli0@outlook.com>
2025-05-06 12:59:02 +0300

    Redirecting the report to the main PGC Report table

--- lib/services/airtable_services.dart
+++ lib/services/airtable_services.dart
@@ -1,5 +1,6 @@
-import '../utilities/keys.dart';
 import 'package:airtable_crud/airtable_plugin.dart';
+
+import '../utilities/keys.dart';
 
 String apiKey = airtableAccessToken;
 
@@ -10,7 +11,7 @@ String kUGGardensBaseID = "appJBOIeM2ZA5nhnV";
 String kUGCurrentGardensBaseID = "appoW7X8Lz3bIKpEE";
 String kPGCsTable = "PGCs";
 String kPGCReportTable =
-    "PGC Report App Test"; //TOD: change the table name to the correct one
+    "PGC Report"; //TOD: change the table name to the correct one
 
 AirtableCrud uGGardensBase = AirtableCrud(apiKey, kUGGardensBaseID);
 AirtableCrud currentGardensBase = AirtableCrud(apiKey, kUGCurrentGardensBaseID);
