4afbd13a813d4bd4f6c162d71069ba526b2cc956
katoemma <emmanuelkato39@gmail.com>
2025-05-19 08:16:39 +0300

    UPDATE: added loading skeleton while updating the app

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:kijani_pgc_app/components/app_drawer.dart';
+import 'package:kijani_pgc_app/components/home_appbar.dart';
 import 'package:kijani_pgc_app/components/reusable_body.dart';
 import 'package:kijani_pgc_app/components/widgets/list_tile.dart';
 import 'package:kijani_pgc_app/controllers/user_controller.dart';
@@ -25,65 +26,14 @@ class HomeScreen extends StatelessWidget {
     return Obx(() {
       final user = User.fromJson(userController.branchData);
       final parishes = userController.parishes;
+      var isLoading = userController.isHomeScreenLoading.value;
 
       return Scaffold(
         backgroundColor: const Color(0xFFF5F5F5),
-        appBar: AppBar(
-          centerTitle: true,
-          leading: Builder(
-            builder: (context) => GestureDetector(
-              onTap: () => Scaffold.of(context).openDrawer(),
-              child: const HugeIcon(
-                icon: HugeIcons.strokeRoundedMenu02,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                greetings.formattedDate(),
-                style: GoogleFonts.roboto(fontSize: 14, color: kGreyText),
-              ),
-            ],
-          ),
-          actions: [
-            GestureDetector(
-              onTap: () => Get.toNamed(Routes.UNSYCEDDATA),
-              child: Container(
-                margin: const EdgeInsets.only(right: 18),
-                child: Obx(() {
-                  return badges.Badge(
-                    showBadge: userController.unsyncedReports > 0,
-                    badgeContent: Text(
-                      userController.unsyncedReports.toString(),
-                      style: GoogleFonts.lato(color: Colors.white),
-                    ),
-                    child: const Icon(HugeIcons.strokeRoundedDatabaseSync01),
-                  );
-                }),
-              ),
-            )
-          ],
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(20.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Hi, ${user.name}",
-                style: GoogleFonts.roboto(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
-          backgroundColor: Colors.white,
-        ),
+        appBar: isLoading
+            ? const CustomHomeAppBarSkeleton()
+            : CustomHomeAppBar(username: user.name),
         drawer: CustomDrawer(),
-
-        /// ✅ Pull-to-Refresh Wrapper
         body: RefreshIndicator(
           color: Colors.green,
           onRefresh: () async {
@@ -95,31 +45,39 @@ class HomeScreen extends StatelessWidget {
               backgroundColor: Colors.green,
             );
           },
-          child: ReusableScreenBody(
-            listTitle: "Assigned Parishes",
-            gridItems: [
-              GridItem(
-                title: "Parishes",
-                value: parishes.length,
-                icon: HugeIcons.strokeRoundedLocation03,
-                color: kijaniBlue,
-              ),
-              GridItem(title: "", value: 0, icon: null, color: kijaniBrown),
-              GridItem(title: "", value: 0, icon: null, color: Colors.black),
-              GridItem(title: "", value: 0, icon: null, color: kijaniGreen),
-            ],
-            items: parishes,
-            itemBuilder: (context, parish, index) => CustomListItem(
-              title: "${parish.name} Parish",
-              subtitle: "{parish.groupIDs.length} Groups",
-              trailing: const Icon(HugeIcons.strokeRoundedArrowRight01,
-                  color: Colors.black),
-              onTap: () {
-                Get.toNamed(Routes.PARISH, arguments: {'parish': parish.id});
-                print("Parish To Open: ${parish.name}");
-              },
-            ),
-          ),
+          child: isLoading
+              ? const ReusableScreenBodySkeleton()
+              : ReusableScreenBody(
+                  listTitle: "Assigned Parishes",
+                  gridItems: [
+                    GridItem(
+                      title: "Parishes",
+                      value: parishes.length,
+                      icon: HugeIcons.strokeRoundedLocation03,
+                      color: kijaniBlue,
+                    ),
+                    GridItem(
+                        title: "", value: 0, icon: null, color: kijaniBrown),
+                    GridItem(
+                        title: "", value: 0, icon: null, color: Colors.black),
+                    GridItem(
+                        title: "", value: 0, icon: null, color: kijaniGreen),
+                  ],
+                  items: parishes,
+                  itemBuilder: (context, parish, index) => CustomListItem(
+                    title: "${parish.name} Parish",
+                    subtitle: "{parish.groupIDs.length} Groups",
+                    trailing: const Icon(HugeIcons.strokeRoundedArrowRight01,
+                        color: Colors.black),
+                    onTap: () {
+                      Get.toNamed(Routes.PARISH, arguments: {
+                        'parish': parish.id,
+                        'name': parish.name
+                      });
+                      print("Parish To Open: ${parish.name}");
+                    },
+                  ),
+                ),
         ),
       );
     });
